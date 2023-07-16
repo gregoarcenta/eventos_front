f694fc83df387e5dfcde82c5daf9a8759c210f6f
Gregory Arcentales <gregoarcenta@gmail.com>
2023-07-16 03:29:45 -0500

    Se agrega responsive a carousel swiper

--- src/app/client/modules/inicio/components/card-features-upcoming/card-features-upcoming.component.ts
+++ src/app/client/modules/inicio/components/card-features-upcoming/card-features-upcoming.component.ts
@@ -1,21 +1,62 @@
-import { environment } from './../../../../../../environments/environment';
-import { Event } from './../../../../../interfaces/event';
-import { Component, Input, OnInit } from '@angular/core';
+import { environment } from "./../../../../../../environments/environment";
+import { Event } from "./../../../../../interfaces/event";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { Swiper } from "swiper";
 
 @Component({
-  selector: 'app-card-features-upcoming',
-  templateUrl: './card-features-upcoming.component.html',
-  styleUrls: ['./card-features-upcoming.component.scss']
+  selector: "app-card-features-upcoming",
+  templateUrl: "./card-features-upcoming.component.html",
+  styleUrls: ["./card-features-upcoming.component.scss"],
 })
-export class CardFeaturesUpcomingComponent implements OnInit {
-
-  @Input() dataEvents:Event[] = []
+export class CardFeaturesUpcomingComponent implements OnInit, AfterViewInit {
+  @Input() dataEvents: Event[] = [];
+  @ViewChild("swiper") swiperRef:
+    | ElementRef<HTMLElement & { swiper?: Swiper } & { initialize: () => void }>
+    | undefined;
+  private swiper?: Swiper;
 
   public url = `${environment.url}/upload/eventos`;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const swiperEl = Object.assign(this.swiperRef!.nativeElement, {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+        clickable: true,
+        dynamicBullets: true,
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: true,
+      },
+      breakpoints: {
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+      },
+    });
+    swiperEl.initialize();
+
+    this.swiper = this.swiperRef!.nativeElement.swiper;
   }
-
 }
