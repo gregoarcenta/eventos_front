c910630f28f4798a1df2c11c1ca05e1d35d39981
Gregory Arcentales <gregoarcenta@gmail.com>
2024-02-19 19:12:57 -0500

    solucion bug no se muestra loader al mostrar un evento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
+import { Subject } from "rxjs";
 import { SpinnerStore } from "./core/services/store/spinner.store";
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  changeDetection:ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
-  active = false;
-  constructor(public spinner: SpinnerStore, private cdr: ChangeDetectorRef) {}
-  ngOnInit(): void {
+export class AppComponent {
+  active = new Subject<boolean>();
+  constructor(public spinner: SpinnerStore, private cdr: ChangeDetectorRef) {
     this.spinner.$active.subscribe((value) => {
-      this.active = value;
-      this.cdr.detectChanges();
+      this.active.next(value);
+      // this.cdr.detectChanges();
     });
   }
 }
--- src/app/client/modules/eventos/pages/evento/evento.component.ts
+++ src/app/client/modules/eventos/pages/evento/evento.component.ts
@@ -1,4 +1,4 @@
-import { EventService } from './../../../../../core/services/api/event.service';
+import { EventService } from "./../../../../../core/services/api/event.service";
 import { MapStore } from "../../../../../core/services/store/map.store";
 import { PlaceStore } from "../../../../../core/services/store/place.store";
 import { environment } from "./../../../../../../environments/environment";
@@ -7,6 +7,7 @@ import { IEvent } from "../../../../../core/interfaces/Event";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "app-evento",
@@ -16,6 +17,7 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 export class EventoComponent implements OnInit {
   public eventId: number | null = null;
   public event: IEvent | null = null;
+  ev = new Subject<IEvent>();
 
   get imageURL(): string {
     return `${environment.url}/upload/eventos/${this.event!.img}`;
@@ -50,23 +52,22 @@ export class EventoComponent implements OnInit {
     this.activatedRoute.params.subscribe(
       (params) => (this.eventId = params["evento"])
     );
-  }
-
-  ngOnInit(): void {
     this.eventService.getEventPublishById(this.eventId!).subscribe({
       next: (response) => {
+        this.ev.next(response.data);
         this.event = response.data;
         this.place.setPlaceLocation(
           this.event.place.direction!.lng,
           this.event.place.direction!.lat
         );
-
       },
       error: (error) => {
         this.router.navigateByUrl("/");
       },
     });
   }
+
+  ngOnInit(): void {}
 
   addToCalendar() {
     const eventTitle = encodeURIComponent("Evento: " + this.event!.name);
@@ -88,7 +89,7 @@ export class EventoComponent implements OnInit {
     this.place.getUserLocation(true).then(({ lng, lat }) => {
       const lng_ = this.event!.place.direction!.lng;
       const lat_ = this.event!.place.direction!.lat;
-      this.map.getRouteBetweenPoints([lng, lat],[Number(lng_), Number(lat_)])
+      this.map.getRouteBetweenPoints([lng, lat], [Number(lng_), Number(lat_)]);
     });
     this.modalService.open(map, { fullscreen: true });
   }
